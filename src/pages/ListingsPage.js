65b8859f7d7807375a0897127106f564cec95fff
Chris <winterch32@outlook.com>
2023-03-28 00:48:38 -0700

    created HomePage, ListingsPage, and several component and feature files

---
+++ src/pages/ListingsPage.js
@@ -0,0 +1,12 @@
+import Navbar from "../components/Navbar";
+
+const ListingsPage = () => {
+  return (
+	<div>
+	  <Navbar />
+	  <h1>Listings Page</h1>
+	</div>
+  );
+};
+
+export default ListingsPage;
