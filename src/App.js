2d675653abbdeff0f9b4f23605a869b3e8b1998d
Leo Saban <leodsaban@gmail.com>
2023-04-04 22:02:09 -0600

    Added paths to about and contact page

--- src/App.js
+++ src/App.js
@@ -2,17 +2,16 @@ import './App.css';
 import HomePage from './pages/HomePage';
 import ListingsPage from './pages/ListingsPage';
 import ContactUs from './pages/ContactUs';
-import Header from './components/Header';
 import { Route, Routes } from 'react-router-dom';
+import About from './pages/About';
 
 function App() {
   return (
     <div className="App">
-      {/* <Header /> */}
         <Routes>
           <Route path='/' element={<HomePage />} />
           <Route path='listings' element={<ListingsPage />} />
-          {/* <Route path='about' element={<About />} /> */}
+          <Route path='about' element={<About />} />
           <Route path='contact' element={<ContactUs />} />
         </Routes>
     </div>
--- src/pages/ContactUs.js
+++ src/pages/ContactUs.js
@@ -1,4 +1,16 @@
 import ContactPage from '../components/ContactPage';
+import TopNavbar from '../components/TopNavbar';
+import { Row } from "reactstrap";
 
+const ContactUs = () => {
+    return (
+      <div>
+        <Row>
+            <TopNavbar />
+        </Row>
+        <ContactPage />
+      </div>
+    );
+  };
 
-export default ContactPage;
+export default ContactUs;
