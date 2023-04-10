0ce6d5748ee5082ea55c2ba96130c7c0b45a85ea
Chris <winterch32@outlook.com>
2023-04-09 22:21:51 -0700

    Added image banner and search bar to home page

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,13 +1,32 @@
 import TopNavbar from "../components/TopNavbar";
-import { Col, Row } from "reactstrap";
+import { Row } from "reactstrap";
+import Banner from "../assets/img/homepagebanner.png";
 
 const HomePage = () => {
   return (
     <div>
       <TopNavbar />
-      <Row>
-        <h1>Home Page</h1>
-      </Row>
+      <div className="banner-container">
+        <img
+          src={Banner}
+          alt="beachside banner"
+          style={{ width: "100%", height: "30vh", objectFit: "cover" }}
+        />
+        <div>
+          <h2 className="banner-text">Book your next favorite vacation rental spot</h2>
+        </div>
+        <div className="search-container">
+          <input
+            type="text"
+            placeholder="Enter your next destination..."
+            className="search-bar"
+          />
+          <button className="search-button">
+            <i class="fa-solid fa-magnifying-glass"></i>
+          </button>
+        </div>
+      </div>
+      <Row></Row>
     </div>
   );
 };
