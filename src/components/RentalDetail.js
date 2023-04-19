036e2fa4304af55f8a8729b01fbea61f3167d4a2
Chris <winterch32@outlook.com>
2023-04-18 21:24:35 -0700

    added more links to Rentals component

--- src/components/RentalDetail.js
+++ src/components/RentalDetail.js
@@ -4,44 +4,68 @@ const RentalDetail = ({ rental }) => {
   const { image, name, additionalImages, link } = rental;
 
   const handleClick = () => {
-    const url =
-      "https://www.airbnb.com/rooms/40711723?check_in=2023-05-13&federated_search_id=f5dd3120-89e1-4c93-b8d4-d620ddf1104d&source_impression_id=p3_1677813421_ONKaUPjEjqCJG9eo&guests=1&adults=1&check_out=2023-05-20";
-    window.location.href = url;
+    const url = {link};
+    window.open(url, "_blank");
   };
 
   return (
     <div className="detail-page-container">
-        <Row>
-          <Col sm="6" md="4" lg="5" className="m-4">
-            <Card>
-              <div className="left-image-container">
-                <CardImg className="detail-card-left" src={image} alt={name} />
-              </div>
-            </Card>
-          </Col>
-          <Col sm="6" md="5" lg="6" className="m-4">
-            <Row>
-              {additionalImages.map((image, index) => (
-                <Col key={index} sm="6" className="mb-2">
-                  <Card>
-                    <CardImg
-                      top
-                      width="100%"
-                      src={image}
-                      alt={`Additional image ${index + 1}`}
-                      className="additional-images"
-                    />
-                  </Card>
-                </Col>
-              ))}
-            </Row>
-            <Row>
-              <Col sm="6" md="5" lg="3" className="m-4 hover-cursor">
-                <button onClick={handleClick}>Book Rental</button>
+      <Row>
+        <Col sm="6" md="5" lg="5" className="m-4">
+          <Card>
+            <div className="left-image-container">
+              <CardImg className="detail-card-left" src={image} alt={name} />
+            </div>
+          </Card>
+        </Col>
+        <Col sm="6" md="5" lg="6" className="m-4">
+          <Row>
+            {additionalImages.map((image, index) => (
+              <Col key={index} sm="6" className="mb-2">
+                <Card>
+                  <CardImg
+                    top
+                    width="100%"
+                    src={image}
+                    alt={`Additional image ${index + 1}`}
+                    className="additional-images"
+                  />
+                </Card>
               </Col>
-            </Row>
-          </Col>
-        </Row>
+            ))}
+          </Row>
+          <Row>
+            <Col
+              sm="6"
+              md="5"
+              lg="3"
+              className="m-4 hover-cursor"
+              style={{
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
+              }}
+            >
+              <button onClick={handleClick} className="button">
+                Book Rental
+              </button>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+      <Row>
+        <Col
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <div>
+            <h3>{name}</h3>
+          </div>
+        </Col>
+      </Row>
     </div>
   );
 };
