3bdac430ddc5c1984b147d8b4bce61a62b002f26
Leo Saban <leodsaban@gmail.com>
2023-04-20 10:51:05 -0600

    added a few more things to the about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,6 +1,7 @@
 import TopNavbar from "../components/TopNavbar";
 import {Col, Row, Container, Card, CardBody, CardHeader} from 'reactstrap';
-import Walk from '../assets/img/walk.png'
+import wheel from '../assets/img/wheel.png';
+import beachtent from '../assets/img/beachtent.jpeg';
 
 const About = () => {
     return (
@@ -9,37 +10,36 @@ const About = () => {
         <Container>
           <Row className="Abouts">
             <Col sm='12'>
-                <img className="Pic" src={Walk} />
-                <h1 className="Text">About Us</h1>
+            <h1 className="Text">About Us</h1>
+                <img className="Pic" src={wheel} />
                 <p className="Contentp">Our mission is to find every <br />family the space they need to relax, 
                           reconnect,<br /> and enjoy precious time away together.</p>
             </Col>
           </Row>
           <Row>
             <Col>
-              <div>
+              <div className="Believe">
               <h1 className="Content">We believe in family connection</h1>
               </div>
             </Col>
           </Row>
           <Col>
               <Card className='bg-light mt-3'>
-                  <CardBody>
+                  <CardBody className="Contentpg">
                       <blockquote className='blockquote'>
-                      <p className="Contentpg">We need each other now more than ever, and we all want more quality time with the people we love. Our focus is on the importance of connection and the joy that celebrating meaningful moments together brings. 
+                      <p >We need each other now more than ever, and we all want more quality time with the people we love. Our focus is on the importance of connection and the joy that celebrating meaningful moments together brings. 
                                   That’s the magic we’re trying to capture. That’s why we’re here.</p>          
-                          <footer className='blockquote-footer'>
-                                By Leo, and Chris{' '}
-                           </footer>
                       </blockquote>
                   </CardBody>
               </Card>
+          </Col>
+          <Row>
+          <Col sm='12'>
+                <h1 className="Text">Our Motto</h1>
+                <img className="Pic2" src={beachtent} />
+                <p className="Contentps">Collect Moments, <br />Not Things.</p>
             </Col>
-            <Row className='row-content'>
-                <Col xs='12'>
-                    <h3>Community Partners</h3>
-                </Col>
-            </Row>
+          </Row>
           </Container>
 
 
